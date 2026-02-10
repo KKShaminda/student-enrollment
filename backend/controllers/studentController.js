1113f8240653da0d99af6cddbb98783c5e8127f1
KKShaminda <shamindakaveesha72@gmail.com>
2026-02-10 17:21:31 +0530

    Added deleteStudent function

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -56,3 +56,20 @@ res.status(200).json(student);
 res.status(500).json({ message: error.message });
 }
 };
+
+//Delete /students/:id
+exports.deleteStudent = async (req, res) => {
+try {
+const student = await Student.findByIdAndDelete(req.params.id);
+
+
+if (!student) {
+return res.status(404).json({ message: 'Student not found' });
+}
+
+
+res.status(200).json({ message: 'Student deleted successfully' });
+} catch (error) {
+res.status(500).json({ message: error.message });
+}
+};
--- backend/routes/student.routes.js
+++ backend/routes/student.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getAllStudents, createStudent, updateStudent } = require('../controllers/studentController');
+const { getAllStudents, createStudent, updateStudent, deleteStudent } = require('../controllers/studentController');
 
 router.get('/', getAllStudents);
 router.post('/', createStudent);
 router.put('/:id', updateStudent);
+router.delete('/:id', deleteStudent);
 
 module.exports = router;
 
